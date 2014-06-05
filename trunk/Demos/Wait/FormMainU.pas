5b45772f9e42c089dd48e304721253be4c99b23a
olaf.monien@gmail.com <olaf.monien@gmail.com@cb74034d-717f-50cd-a2c3-1ac1f0c00770>
2014-06-05 00:46:54 +0000

    TWait implemented

---
+++ trunk/Demos/Wait/FormMainU.pas
@@ -0,0 +1,59 @@
+unit FormMainU;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Layouts, FMX.Objects;
+
+type
+  TFormMain = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Rectangle1: TRectangle;
+    Timer1: TTimer;
+    procedure Button1Click(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  FormMain: TFormMain;
+
+implementation
+
+uses
+  DX.FMX.Wait, System.IOUtils, Form2U;
+
+{$R *.fmx}
+
+type
+  TFormTest = class(TCustomPopupForm)
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+procedure TFormMain.Button1Click(Sender: TObject);
+begin
+  TWait.Start('Loading data...');
+  Timer1.Enabled := true;
+end;
+
+procedure TFormMain.Timer1Timer(Sender: TObject);
+begin
+  TWait.Stop;
+  Timer1.Enabled := false;
+end;
+
+end.
---
+++ trunk/Demos/Wait/Wait.dpr
@@ -0,0 +1,21 @@
+program Wait;
+
+{$R 'Wheel.res' '..\..\Wheel.rc'}
+{$R *.dres}
+
+uses
+  System.StartUpCopy,
+  FMX.MobilePreview,
+  FMX.Forms,
+  FormMainU in 'FormMainU.pas' {FormMain},
+  DX.FMX.Wait in '..\..\DX.FMX.Wait.pas',
+  Form2U in 'Form2U.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFormMain, FormMain);
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
