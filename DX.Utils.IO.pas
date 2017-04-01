83402f00e83233bbd95d2f0e59f27905f8863236
Olaf Monien <olaf@monien.net>
2017-04-01 15:13:32 +0200

    initial AppPath added

---
+++ DX.Utils.IO.pas
@@ -0,0 +1,54 @@
+unit DX.Utils.IO;
+
+interface
+
+uses
+  System.Classes, System.SysUtils,
+{$IFDEF MACOS}
+  Macapi.Helpers,
+{$IFDEF IOS}
+  iOSApi.Foundation,
+{$ELSE !IOS}
+  Macapi.Foundation,
+  Macapi.Cocoatypes,
+{$ENDIF IOS}
+{$ENDIF MACOS}
+  System.IOUtils;
+
+type
+
+  TPathHelper = record helper for TPath
+  public
+    class function GetAppPath: string; inline; static;
+  end;
+
+implementation
+
+{ TPathHelper }
+
+class function TPathHelper.GetAppPath: string;
+{$IFDEF MSWINDOWS}
+begin
+  Result := ExtractFilePath(ParamStr(0));
+end;
+{$ENDIF}
+{$IFDEF POSIX}
+{$IFDEF MACOS}
+begin
+  Result := Tpath.GetDirectoryName(NSStrToStr( TNSBundle.Wrap(TNSBundle.OCClass.mainBundle).bundlePath));
+end;
+{$ENDIF MACOS}
+{$IFDEF ANDROID}
+begin
+  raise Exception.Create('Not implemented yet!');
+  // Result := AndroidApi.IOUtils.GetLibraryPath;
+end;
+{$ENDIF ANDROID}
+{$IFDEF LINUX}
+begin
+  raise Exception.Create('Not implemented yet!');
+end;
+{$ENDIF LINUX}
+{$ENDIF POSIX}
+
+end.
