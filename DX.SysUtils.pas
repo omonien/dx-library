58e4bd46fc685a731d211a3f5c462f5183c1bbef
Olaf Monien <olaf@monien.net>
2018-04-28 14:13:31 +0200

    DUnitX Tests added. SHA256 and SHA512 not working yet

--- DX.SysUtils.pas
+++ DX.SysUtils.pas
@@ -6,32 +6,123 @@ uses
   System.Classes, System.SysUtils;
 
 type
+{$SCOPEDENUMS ON}
+{$REGION 'Documentation'}
+  /// <summary>
+  /// Implemented Hash algorithms.
+  /// </summary>
+  /// <remarks>
+  /// IMPORTANT: MD5 and SHA1 are both UNSAFE in terms of cryptography. Use
+  /// for checksums etc only!
+  /// </remarks>
+{$ENDREGION}
+  THashAlgorithm = (MD5, SHA1, SHA256, SHA512);
+{$SCOPEDENUMS OFF}
+
   THash = class(TObject)
+  protected
   public
-   class function MD5(const fileName: string): string;
+{$REGION 'Documentation'}
+    /// <summary>
+    /// returns hash/checksum for a stream.
+    /// </summary>
+{$ENDREGION}
+    class function Hash(const AStream: TStream; AAlgorithm: THashAlgorithm): string; overload;
+{$REGION 'Documentation'}
+    /// <summary>
+    /// returns hash/checksum for a string.
+    /// </summary>
+{$ENDREGION}
+    class function Hash(const AValue: string; AAlgorithm: THashAlgorithm): string; overload;
+{$REGION 'Documentation'}
+    /// <summary>
+    /// returns hash/checksum for a file.
+    /// </summary>
+{$ENDREGION}
+    class function HashForFile(const AFileName: string; AAlgorithm: THashAlgorithm): string;
+{$REGION 'Documentation'}
+    /// <summary>
+    /// MD5 Hash for a file.
+    /// </summary>
+{$ENDREGION}
+    class function MD5ForFile(const AFileName: string): string;
+{$REGION 'Documentation'}
+    /// <summary>
+    /// SHA1 Hash for a file.
+    /// </summary>
+{$ENDREGION}
+    class function SHA1ForFile(const AFileName: string): string;
   end;
 
 implementation
 
 uses
-  IdHashMessageDigest, IdHash;
+  IdHash, IdHashMessageDigest, IdHashSHA;
 
-// returns MD5 has for a file
-class function THash.MD5(const fileName: string): string;
+class function THash.Hash(const AStream: TStream; AAlgorithm: THashAlgorithm): string;
 var
-  LDigest: TIdHashMessageDigest5;
-  LStream: TFileStream;
+  LHash: TIdHash;
 begin
-  LDigest := nil;
-  LStream := nil;
+  LHash := nil;
   try
-    LDigest := TIdHashMessageDigest5.Create;
-    LStream := TFileStream.Create(fileName, fmOpenRead OR fmShareDenyWrite);
-    result := LDigest.HashStreamAsHex(LStream);
+    case AAlgorithm of
+      THashAlgorithm.MD5:
+        LHash := TIdHashMessageDigest5.Create;
+      THashAlgorithm.SHA1:
+        LHash := TIdHashSHA1.Create;
+      THashAlgorithm.SHA256:
+        LHash := TIdHashSHA256.Create;
+      THashAlgorithm.SHA512:
+        LHash := TIdHashSHA512.Create;
+    end;
+    result := LHash.HashStreamAsHex(AStream);
+  finally
+    FreeAndNil(LHash);
+  end;
+
+end;
+
+class function THash.Hash(const AValue: string; AAlgorithm: THashAlgorithm): string;
+var
+  LStream: TStringStream;
+begin
+  LStream := TStringStream.Create(AValue);
+  try
+    result := Hash(LStream, AAlgorithm);
   finally
     FreeAndNil(LStream);
-    FreeAndNil(LDigest);
   end;
+
+end;
+
+class function THash.HashForFile(const AFileName: string; AAlgorithm: THashAlgorithm): string;
+var
+  LStream: TFileStream;
+begin
+  LStream := nil;
+  try
+    if FileExists(AFileName) then
+    begin
+      LStream := TFileStream.Create(AFileName, fmOpenRead OR fmShareDenyWrite);
+      result := Hash(LStream, AAlgorithm);
+    end
+    else
+    begin
+      result := '';
+    end;
+  finally
+    FreeAndNil(LStream);
+  end;
+end;
+
+class function THash.MD5ForFile(const AFileName: string): string;
+begin
+  result := HashForFile(AFileName, THashAlgorithm.MD5);
+end;
+
+class function THash.SHA1ForFile(const AFileName: string): string;
+begin
+  result := HashForFile(AFileName, THashAlgorithm.SHA1);
 end;
 
 end.
---
+++ Tests/SysUtilsTests.pas
@@ -0,0 +1,95 @@
+unit SysUtilsTests;
+
+interface
+
+uses
+  DUnitX.TestFramework;
+
+type
+
+  [TestFixture]
+  THashTests = class(TObject)
+  private
+    FInputString: string;
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+    [Test]
+    procedure TestMD5;
+    [Test]
+    procedure TestSHA1;
+    [Test]
+    procedure TestSHA256;
+    [Test]
+    procedure TestSHA512;
+
+    // Test with TestCase Attribute to supply parameters.
+    // [Test]
+    // [TestCase('TestA','1,2')]
+    // [TestCase('TestB','3,4')]
+    procedure Test2(const AValue1: Integer; const AValue2: Integer);
+  end;
+
+implementation
+
+uses
+  DX.SysUtils, System.SysUtils;
+
+procedure THashTests.Setup;
+begin
+  FInputString := 'Hello World';
+end;
+
+procedure THashTests.TearDown;
+begin
+end;
+
+procedure THashTests.TestMD5;
+var
+  LHash: string;
+begin
+  // See: http://www.sha1-online.com
+  // Or: https://hashgenerator.de/
+  LHash := THash.Hash(FInputString, THashAlgorithm.MD5);
+  Assert.AreEqual(LHash, 'B10A8DB164E0754105B7A99BE72E3FE5');
+end;
+
+procedure THashTests.TestSHA1;
+var
+  LHash: string;
+  LInput: string;
+begin
+  LHash := THash.Hash(FInputString, THashAlgorithm.SHA1);
+  Assert.AreEqual(LHash.ToLower, '0a4d55a8d778e5022fab701977c5d840bbc486d0');
+end;
+
+procedure THashTests.TestSHA256;
+var
+  LHash: string;
+  LInput: string;
+begin
+  LHash := THash.Hash(FInputString, THashAlgorithm.SHA256);
+  Assert.AreEqual(LHash.ToLower, 'a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e');
+end;
+
+procedure THashTests.TestSHA512;
+var
+  LInput: string;
+  LHash: string;
+begin
+  LHash := THash.Hash(FInputString, THashAlgorithm.SHA512);
+  Assert.AreEqual(LHash.ToLower,
+    '2c74fd17edafd80e8447b0d46741ee243b7eb74dd2149a0ab1b9246fb30382f27e853d8585719e0e67cbda0daa8f51671064615d645ae27acb15bfb1447f459b');
+end;
+
+procedure THashTests.Test2(const AValue1: Integer; const AValue2: Integer);
+begin
+end;
+
+initialization
+
+TDUnitX.RegisterTestFixture(THashTests);
+
+end.
