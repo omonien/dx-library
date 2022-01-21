dba90986016e06de45342b0d2e5aeb0d337eb9dc
Olaf Monien <olaf@monien.net>
2022-01-21 12:06:28 +0100

    Interfaced Singlton added

--- DX.Classes.Singleton.pas
+++ DX.Classes.Singleton.pas
@@ -24,11 +24,37 @@ type
     class constructor Create;
     class destructor Destroy;
     /// <summary>
-    ///   The default instance.
+    /// The default instance.
     /// </summary>
     class function Default: T;
     /// <summary>
-    ///   Alias of Default
+    /// Alias of Default
+    /// </summary>
+    class function Instance: T;
+  end;
+
+type
+  /// <summary>
+  /// Interfaced variant of TInterfacedSingleton.
+  /// </summary>
+  /// <typeparam name="T">
+  /// T is the actual class that descends from TInterfacedSingleton
+  /// </typeparam>
+  /// <remarks>
+  /// Do not create direct instances of TInterfacedSingleton - only descend from it.
+  /// </remarks>
+  TInterfacedSingleton<T: Class> = class abstract(TInterfacedObject)
+  strict private
+    class var FDefaultInstance: T;
+  public
+    class constructor Create;
+    class destructor Destroy;
+    /// <summary>
+    /// The default instance.
+    /// </summary>
+    class function Default: T;
+    /// <summary>
+    /// Alias of Default
     /// </summary>
     class function Instance: T;
   end;
@@ -60,7 +86,31 @@ end;
 
 class function TSingleton<T>.Instance: T;
 begin
- result := Default;
+  result := Default;
+end;
+
+class constructor TInterfacedSingleton<T>.Create;
+begin
+  FDefaultInstance := nil;
+end;
+
+class function TInterfacedSingleton<T>.Default: T;
+begin
+  if not Assigned(FDefaultInstance) then
+  begin
+    // Some RTTI magic is needed here. T.Create cannot be called directly.
+    FDefaultInstance := TClassConstructor.Construct<T>;
+  end;
+  result := FDefaultInstance;
+end;
+
+class destructor TInterfacedSingleton<T>.Destroy;
+begin
+end;
+
+class function TInterfacedSingleton<T>.Instance: T;
+begin
+  result := Default;
 end;
 
 end.
