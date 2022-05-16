5f75444e322d7879f34943add48816dfc04e00a2
Olaf Monien <olaf@monien.net>
2022-05-16 18:44:41 +0200

    Added paginating

--- DX.Classes.Collections.pas
+++ DX.Classes.Collections.pas
@@ -25,11 +25,55 @@ type
     /// A list containing instances of type T.
     /// </param>
     class procedure RemoveDuplicates(AList: TList<T>);
+
+    /// <summary>
+    /// "Paginates" the List by returing only the elements that belong to the given page.
+    /// </summary>
+    class procedure Paginate(AList: TList<T>; APage, APageSize: integer); static;
   end;
 
 implementation
 
+uses
+  System.Math;
+
 { TListHelper<T> }
+
+
+class procedure TListHelper<T>.Paginate(
+  AList: TList<T>;
+  APage, APageSize: integer);
+begin
+  if APage < 1 then
+    raise Exception.Create('Page must be larger than 0!');
+
+  if APageSize < 1 then
+    raise Exception.Create('PageSize must be larger than 0!');
+
+  var
+  LCount := AList.Count;
+  var
+  LPages := LCount div APageSize;
+  // APage/LPages are 1-based
+  if (LCount mod APageSize) > 0 then
+  begin
+    inc(LPages);
+  end;
+  // APage := Min(APage, LPages);
+  // LFrom is 0-based
+  var
+  LFrom := (APage - 1) * APageSize;
+
+  // Alle Elemente vor LFrom entfernen
+  AList.DeleteRange(0, Min(AList.Count, LFrom));
+  // Alle Elemente nach einer PageSize entfernen
+  if AList.Count > APageSize then
+  begin
+    AList.DeleteRange(APageSize, AList.Count - APageSize);
+  end;
+end;
+
+
 
 class procedure TListHelper<T>.RemoveDuplicates(AList: TList<T>);
 begin
