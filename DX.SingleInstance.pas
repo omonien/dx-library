16a17f909cbe58330f783fc5f61a8709ba5c3551
Olaf Monien <olaf@monien.net>
2025-02-25 18:51:16 +0100

    Modified to offer functionality via class method
    Avoids orphaned, useless TSingleInstance instance

--- DX.SingleInstance.pas
+++ DX.SingleInstance.pas
@@ -12,32 +12,32 @@ type
   /// </summary>
   TSingleInstance = class
   private
-    FMutex: THandle;
-    FAppId: string;
-    /// <summary>
-    /// Checks if an instance with the same AppId is already running.
-    /// </summary>
-    /// <returns>True if another instance is running, False otherwise.</returns>
-    function IsRunning: Boolean;
+    class var FAppId: string;
+    class var FMutex: THandle;
+      /// <summary>
+      /// Checks if an instance with the same AppId is already running.
+      /// </summary>
+      /// <returns>True if another instance is running, False otherwise.</returns>
+    class function IsRunning: Boolean;
   public
     /// <summary>
-    /// Creates a new instance of TSingleInstance and terminates immediately if another instance with the same ID is running.
+    /// Checks if an instance with the same AppID is already running and terminates the current process immediately if so.
     /// </summary>
     /// <param name="AAppId">A unique identifier for the application. Should be unique across all applications.</param>
     /// <remarks>
-    /// Use this constructor as the first line after "begin" in your .dpr file to ensure single instance behavior.
+    /// Use this method as the first line after "begin" in your .dpr file to ensure single instance behavior.
     /// If another instance with the same AAppId is detected, the current process will terminate immediately.
     /// </remarks>
-    constructor Create(const AAppId: string);
+    class procedure Check(const AAppId: string);
     /// <summary>
-    /// Destroys the instance and releases the mutex handle if it was created.
+    /// Cleans up and releases the mutex handle if it was created.
     /// </summary>
-    destructor Destroy; override;
+    class destructor Destroy;
   end;
 
 implementation
 
-constructor TSingleInstance.Create(const AAppId: string);
+class procedure TSingleInstance.Check(const AAppId: string);
 begin
   inherited Create;
   FAppId := AAppId;
@@ -45,25 +45,25 @@ begin
     Halt; // Terminate the application if another instance is detected
 end;
 
-destructor TSingleInstance.Destroy;
+class destructor TSingleInstance.Destroy;
 begin
   if FMutex <> 0 then
     CloseHandle(FMutex); // Release the mutex handle if it exists
-  inherited Destroy;
 end;
 
-function TSingleInstance.IsRunning: Boolean;
+class function TSingleInstance.IsRunning: Boolean;
 var
-  MutexName: string;
+  LMutexName: string;
 begin
   // Unique name for the mutex based on AppId
-  MutexName := 'Global\' + FAppId + '_SingleInstanceMutex';
+  LMutexName := 'Global\' + FAppId + '_SingleInstanceMutex';
 
   // Create the mutex
-  FMutex := Winapi.Windows.CreateMutex(nil, True, PChar(MutexName));
+  FMutex := Winapi.Windows.CreateMutex(nil, True, PChar(LMutexName));
 
   // Check if the mutex already exists
   Result := (FMutex = 0) or (GetLastError = ERROR_ALREADY_EXISTS);
 end;
 
 end.
+
